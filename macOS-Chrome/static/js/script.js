14478d87b9b7a948885b2cbae7af57df6f802bb4
sysophost <oxides-walkers.0x@icloud.com>
2024-07-08 15:01:33 +1000

    first add

---
+++ macOS-Chrome/static/js/script.js
@@ -0,0 +1,19 @@
+$(document).on("DOMContentLoaded", function() {
+  $("#window").draggable();
+  $("#window").resizable();
+
+  var uuid = self.crypto.randomUUID();
+  $("#domain-path").text(`/o/oauth2/v2/auth/identifier?state=${uuid}&access_type=offline&prompt=consent&scope=`);
+  
+  $("#title-bar").on("dblclick", function(e){
+      $("#window").toggleClass("enlarged");
+  });
+  
+  $("#square").click(function(){
+      $("#window").toggleClass("enlarged");
+  });
+
+  $("#exit").click(function(){
+      $("#window").css("display", "none");
+  });
+});
